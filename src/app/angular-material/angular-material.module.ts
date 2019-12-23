0440aa12334a83c03b9d8f6082ec99ff086d88e9
Toufik <toufik.menad@gmail.com>
2019-12-23 01:03:31 -0800

    testing ssl

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
   ]
 })
 export class AngularMaterialModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { AngularMaterialModule} from'../angular-material/angular-material.module';
+import { from } from 'rxjs';
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class SharedModule { }
