ece6dd5d6838d3af6c6329476f5221846c2907c5
Toufik <toufik.menad@gmail.com>
2019-12-29 17:44:57 -0800

    added logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  {path: 'header',
-  component: HeaderComponent
+  {path: 'full',
+  component: AppComponent
   }
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { AngularMaterialModule} from'../angular-material/angular-material.module';
 import { from } from 'rxjs';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SearchbarComponent],
   imports: [
     CommonModule,
     AngularMaterialModule
+  ],
+  exports: [
+    HeaderComponent,
+    SearchbarComponent
   ]
 })
 export class SharedModule { }
