0f64958105f93b7af6fbae7a6a5b2bcf5c57b3dc
Toufik <toufik.menad@gmail.com>
2020-05-30 18:05:30 -0700

    u t fix

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['ChromeHeadless'],
+    browserDisconnectTimeout: 6000,
     singleRun: true,
     restartOnFileChange: true
   });
